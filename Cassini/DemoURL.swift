d5cb49b66280056bb715391f721dff77b0a8ca67
Olli-Pekka Oras <Olli-Pekka.Oras@conmio.com>
2017-03-14 14:22:30 +0200

    Added loading test imageURL to scrollview

---
+++ Cassini/DemoURL.swift
@@ -0,0 +1,14 @@
+//
+//  DemoURL.swift
+//  Cassini
+//
+//  Created by ooras on 14/03/2017.
+//  Copyright © 2017 oporas. All rights reserved.
+//
+
+import Foundation
+
+struct DemoURL {
+    static let tonni = URL(string: "https://i.ytimg.com/vi/mHvBvkSCx2w/hqdefault.jpg")
+    static let oispa = URL(string: "https://media.riemurasia.net/albumit/mmedia/7u/rnh/6aem/28955/1066601341.jpg")
+}
